9e45f23656badee6f0ff1caddb5e4672e9df9a9c
jkaberon <jkaberon@gmail.com>
2018-09-02 00:02:33 -0500

    asdf

--- text/views.py
+++ text/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import time
 import subprocess
 import os
 # Create your views here.
@@ -15,7 +14,7 @@ def index(request):
 			info.append(request.POST[k])
 	curr_path = os.path.dirname(__file__)
 	c_path = os.path.join(curr_path,'web_courses.py')
-	args = ['python.exe', c_path]
+	args = ['python', c_path]  #'python.exe'
 	args.extend(info)
 	subprocess.Popen(args)
 	return HttpResponse("Your request is being processed. Kick back, relax, and we'll let you know when there's an opening.")
