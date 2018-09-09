4a3586dee3a6354a0d3359220f3bbfaf535149aa
jkaberon <jkaberon@gmail.com>
2018-09-09 00:24:56 -0500

    headless

--- text/web_courses.py
+++ text/web_courses.py
@@ -16,6 +16,7 @@ def everything():
 	
 	chrome_options = Options()
 	chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+	chrome_options.add_argument('--headless')
 	chrome_options.add_argument('--disable-gpu')
 	chrome_options.add_argument('--no-sandbox')
 	browser = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
